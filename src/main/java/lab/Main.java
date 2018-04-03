0125779097fe789520669e31ef096fd16abb3053
Jermaine34343 <Jermaine34343@gmail.com>
2018-04-03 16:54:00 -0400

    Update

--- src/main/java/lab/Main.java
+++ src/main/java/lab/Main.java
@@ -3,5 +3,6 @@ package lab;
 public class Main {
     public static void main(String[] args) {
         System.out.println("It works");
+        System.out.println("For the super negros");
     }
 }
